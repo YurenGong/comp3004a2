8ed22a4a16f0e116adf03f498de6ee083aa81e63
KelMG <gongyurengg@gmail.com>
2021-02-14 17:47:52 +0000

    - final commit with working code

---
+++ comp3004a2/src/main/java/Study/comp3004a2/additionClass.java
@@ -0,0 +1,11 @@
+package Study.comp3004a2;
+
+public class additionClass {
+	public int addOne(int x) {
+		return x+1;
+	}
+	
+	public int addTwoNumber(int x, int y) {
+		return x+y;
+	}
+}
---
+++ comp3004a2/src/test/java/Study/comp3004a2/testSuite.java
@@ -0,0 +1,15 @@
+package Study.comp3004a2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    additionTest.class,
+    additionTest2.class
+ })
+
+
+public class testSuite {
+
+}
