0842c77c0d46f924e698aa5b4dfa4c9f4b4c5dfc
KelMG <gongyurengg@gmail.com>
2021-02-14 17:18:06 +0000

    - new dependencies in the xml file

---
+++ comp3004a2/src/main/java/Study/comp3004a2/App.java
@@ -0,0 +1,13 @@
+package Study.comp3004a2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
